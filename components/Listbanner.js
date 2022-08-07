00e93520ef52b2093eecaa846b39ba8212cc9464
Harun Inam <45066973+HarunInam@users.noreply.github.com>
2022-08-07 21:53:23 +0300

    Bir takım değişikliler

---
+++ components/Listbanner.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Listbanner = () => {
+  return (
+    <section className='mt-[150px] flex flex-row justify-center'>
+        <div className='border-box border-[1px] text-white rounded-2xl border-gray-400'>
+            <h5 className='text-2xl w-2/3 p-3'>
+            Check out the hottest Sale offers
+            </h5>
+            <div className='outline'>
+
+            </div>
+        </div>
+        <div className='text-white'>
+            lel1
+        </div>
+    </section>
+  )
+}
+
+export default Listbanner
