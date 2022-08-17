d6541f35b7bf5ef391138749b30ec84ecc79fabc
Recai Çakıroğlu <recai.cakiroglu@startuphero.es>
2022-08-17 18:01:41 +0300

    bug fix on class property

--- components/Grids.js
+++ components/Grids.js
@@ -2,7 +2,7 @@
 const Grids = () => {
   return (
     <div>
-        <span class="h-1 w-full bg-purple-600 lg:w-1/3">
+        <span className="h-1 w-full bg-purple-600 lg:w-1/3">
             aaaaaaaa
         </span>
     </div>
