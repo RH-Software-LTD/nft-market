45bb4d298290e57c254831a91b72fcfdf23136f8
Recai Çakıroğlu <recai.cakiroglu@startuphero.es>
2022-08-17 18:02:10 +0300

    banners responsive update

--- components/Banner.js
+++ components/Banner.js
@@ -2,27 +2,27 @@ import React from 'react'
 
 const Banner = () => {
     return (
-        <section className='mt-24 w-3/4 flex flex-row justify-center space-x-[100px] items-center '>
-            <div className='flex flex-col w-2/5'>
+        <section className='mt-24 flex flex-col-reverse sm:flex-row justify-center items-center '>
+            <div className='flex flex-col w-10/12 w-2/5'>
                 <span className='font-InterSans uppercase mb-4 text-[15px] leading-[18px] tracking-[3px] font-semibold text-gray-500'>
                     Overline
                 </span>
-                <h1 className='text-6xl mb-6 tracking-[0.2px] font-semibold text-white'>
+                <h1 className='text-3xl lg:text-5xl mb-6 tracking-[0.2px] font-semibold text-white'>
                     Sapien ipsum scelerisque <br /> convallis fusce
                 </h1>
-                <p className='font-InterSans leading-[150%] text-gray-500'>
+                <p className='font-InterSans leading-[150%] text-[#7780A1] text-gray-500'>
                     Ut amet vulputate faucibus vitae semper eget auctor. Diam <br /> tempor pulvinar ultricies dolor feugiat aliquam commodo.
                 </p>
                 <div className='text-white flex flex-row mt-12 gap-4 text-center items-center'>
-                    <a className='text-white whitespace-nowrap w-5/10 rounded-xl bg-[#4745D0] p-3 shadow-lg purple-button' href='#'>
+                    <a className='text-white whitespace-nowrap w-1/5 rounded-xl bg-[#4745D0] p-3 shadow-lg purple-button' href='#'>
                         Get started
                     </a>
-                    <a className='text-gray-600 whitespace-nowrap w-4/10 border-gray-500 border-[1px] p-3 rounded-xl border-opacity-30' href='#'>
+                    <a className='text-gray-600 whitespace-nowrap w-1/5 border-gray-500 border-[1px] p-3 rounded-xl border-opacity-30' href='#'>
                         Learn more
                     </a>
                 </div>
             </div>
-            <div className='w-3/5 flex items-center justify-center'>
+            <div className='sm:w-3/5 flex items-center justify-center'>
                 <img className='w-4/5' src='image-banner.png' />
             </div>
         </section>
--- components/BannerLeft.js
+++ components/BannerLeft.js
@@ -1,44 +1,46 @@
 const BannerLeft = () => {
-  return (
-    <section className='mt-[150px] mb-[100px] flex justify-center'>
-        <div className='flex flew-row space-x-32 items-center text-white'>
-            <div className='w-[684px] h-[616px]'>
-                <img src='left-image.png'/>
-            </div>
-            <div className='flex flex-col w-1/2'>
-                <span className='text-[15px] text-gray-400 uppercase'>overline</span>
-                <h1 className='text-white text-6xl font-semibold leading-tight'>
-                Habitant tristique <br/>aliquam in vel<br/>scelerisque
-                </h1>
-                <p className='text-base leading-loose text-[#7780A1]'>
-                Ut amet vulputate faucibus vitae semper eget auctor. Diam tempor pulvinar <br/> ultricies dolor feugiat aliquam commodo.
-                </p>
-                <div className='flex flex-row space-x-10 mt-8 items-center'>
-                    <div className='flex flex-col'>
-                        <img src='icon.png' className='w-6 h-6'/>
-                        <h6 className='text-white font-semibold mt-3'>Sollicitudin sapien</h6>
-                        <p className='text-gray-700 text-[14px]'>Cursus fermentum</p>
-                    </div>
-                    <div className='flex flex-col'>
-                        <img src='icon-2.png' className='w-6 h-6'/>
-                        <h6 className='text-white font-semibold mt-3'>Pulvinar metus</h6>
-                        <p className='text-gray-700 text-[14px]'>Nunc sed</p>
-                    </div>
+    return (
+        <section className='mt-[150px] mb-[100px] flex justify-center text-center md:text-start'>
+            <div className='flex flex-col sm:flex-row lg:space-x-32 items-center text-white'>
+                <div className='w-4/5 sm:w-1/2'>
+                    <img src='left-image.png' />
                 </div>
-                <div className='mt-[56px] flex flex-row space-x-4 items-center'>
-                    <a className='bg-[#4745D0] p-3 border-2 border-[#4745D0] border-opacity-40 rounded-xl' href='#'>
+                <div className='flex flex-col w-4/5 sm:w-1/2'>
+                    <span className='font-InterSans uppercase mb-4 text-[15px] leading-[18px] tracking-[3px] font-semibold text-gray-500'>
+                        Overline
+                    </span>
+                    <h1 className='text-3xl lg:text-5xl mb-6 tracking-[0.2px] font-semibold text-white'>
+                        Habitant tristique <br />aliquam in vel<br />scelerisque
+                    </h1>
+                    <p className='font-InterSans leading-[150%] text-[#7780A1] text-gray-500'>
+                        Ut amet vulputate faucibus vitae semper eget auctor. Diam tempor pulvinar <br /> ultricies dolor feugiat aliquam commodo.
+                    </p>
+                    <div className='flex flex-row mt-8 items-center'>
+                        <div className='flex flex-col w-1/2 sm:w-1/3 items-center md:items-start'>
+                            <img src='icon.png' className='w-6 h-6' />
+                            <h6 className='text-white font-semibold mt-3'>Sollicitudin sapien</h6>
+                            <p className='text-gray-700 text-[14px]'>Cursus fermentum</p>
+                        </div>
+                        <div className='flex flex-col w-1/2 sm:w-1/3 items-center md:items-start'>
+                            <img src='icon-2.png' className='w-6 h-6' />
+                            <h6 className='text-white font-semibold mt-3'>Pulvinar metus</h6>
+                            <p className='text-gray-700 text-[14px]'>Nunc sed</p>
+                        </div>
+                    </div>
+                    <div className='mt-[56px] flex flex-row gap-2 sm:gap-4 justify-center md:justify-start items-center'>
+                        <a className='bg-[#4745D0] text-center w-1/2 sm:w-1/5 whitespace-nowrap p-3 border-2 border-[#4745D0] border-opacity-40 rounded-xl' href='#'>
                             Get Started
-                    </a>
-                    <a className='border-2 border-gray-400 p-3 rounded-xl' href='#'>
-                        <span className='text-gray-400'>
-                            Learn more
-                        </span>
-                    </a>
+                        </a>
+                        <a className='w-1/2 sm:w-1/5 text-center border-2 border-gray-400 p-3 whitespace-nowrap rounded-xl' href='#'>
+                            <span className='text-gray-400'>
+                                Learn more
+                            </span>
+                        </a>
+                    </div>
                 </div>
             </div>
-        </div>
-    </section>
-  )
+        </section>
+    )
 }
 
 export default BannerLeft
