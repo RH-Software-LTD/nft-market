6836ffa0aee92bf49611ee447a6e8a22e626f306
Recai Çakıroğlu <recai.cakiroglu@startuphero.es>
2022-09-04 22:06:05 +0300

    hero and background refactored.

--- components/Hero.js
+++ components/Hero.js
@@ -12,21 +12,7 @@ const Hero = () => {
           <img src='/hero-frame.png' alt='hero-frame' />
 
           <p className='font-InterSans text-xs sm:text-xl my-4 lg:my-12'>Discover, collect and sell</p>
-          <div className="hidden xl:flex p-3 w-3/4 mx-auto focus:outline-none bg-white rounded-lg">
-            <input className="w-full border-0 focus:outline-none py-2" type="text" placeholder="Items, collections and creators" />
-            <select className="pl-6 pr-6 border-0 border-l border-gray-200 focus:outline-none cursor-pointer" name="" id="">
-              <option data-config-id="opt1">Category</option>
-              <option data-config-id="opt2">Music</option>
-              <option data-config-id="opt3">Art</option>
-            </select>
-            <div className='text-blue-900 py-3 ml-5 px-4 text-xl'>
-              <SearchIcon className='w-6 h-6 rotate-90' />
-            </div>
-            <div>
-              <img />
-              <img />
-            </div>
-          </div>
+
         </div>
       </div>
       <div className='flex flex-row w-1/2 sm:w-1/3  justify-center gap-3'>
